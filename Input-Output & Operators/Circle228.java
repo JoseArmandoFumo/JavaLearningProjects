fe32253684e535a4406e680612a9e2cae2f6dc62
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-15 16:51:08 +0200

    pplication that inputs from the user the radius of a circle as an integer and prints the circle’s diameter, circumference and area using the floating-point value 3.14159 for π.

---
+++ Input-Output & Operators/Circle228.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringValue229 { 
+	public static void main (String[] args) {
+		
+		/* Scanner input = new Scanner(System.in);
+		
+		float r;
+		float diam;
+		float cir;
+		float area;
+
+		System.out.print ("Digite o raio: ");
+		r = input.nextFloat(); */
+
+		System.out.printf ("Diamento = %.2f%n", r*2);
+		System.out.printf ("Circuferencia = %.2f%n", 2*r*Math.PI);
+		System.out.printf ("Area = %.2f", Math.PI*r*r);
+		
+		
+	 
+				}
+
+}
