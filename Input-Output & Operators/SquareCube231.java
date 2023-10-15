f763a025f0a6ad6f88dbbde9d68db21efdc2d1a2
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-15 17:01:07 +0200

    calculates the squares and cubes of the numbers from 0 to 10 and prints the resulting values in table format.

---
+++ Input-Output & Operators/SquareCube231.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SquareCube231 
+{
+	public static void main (String [] args) 
+	  {
+		Scanner input = new Scanner(System.in);
+		int number;
+		int square;
+		int cube;
+		
+		System.out.printf ("%s%s%s%n", "Number      ", "Square     ", "Cube    ");
+		System.out.printf ("%d           %d	       %d%n", 0, 0*0, 0*0*0); 
+		System.out.printf ("%d           %d	       %d%n", 1, 1*1, 1*1*1);
+		System.out.printf ("%d           %d	       %d%n", 2, 2*2, 2*2*2);
+		System.out.printf ("%d           %d	       %d%n", 3, 3*3, 3*3*3);
+		System.out.printf ("%d           %d	       %d%n", 4, 4*4, 4*4*4);
+		System.out.printf ("%d           %d	       %d%n", 5, 5*5, 5*5*5);
+		System.out.printf ("%d           %d	       %d%n", 6, 6*6, 6*6*6);
+		System.out.printf ("%d           %d	       %d%n", 7, 7*7, 7*7*7);
+		System.out.printf ("%d           %d	       %d%n", 8, 8*8, 8*8*8);
+		System.out.printf ("%d           %d	       %d%n", 9, 9*9, 9*9*9);
+		System.out.printf ("%d          %d	       %d%n", 10, 10*10, 10*10*10);
+
+
+
+	}
+
+}	
