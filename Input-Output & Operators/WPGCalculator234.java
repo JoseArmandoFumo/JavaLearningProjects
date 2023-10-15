c325c1e1adec1e1ea987d5e8fed230491d9bd88f
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-15 17:09:16 +0200

    World Population Growth Calculator (Incomplete)

---
+++ Input-Output & Operators/WPGCalculator234.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WPGCalculator234
+{
+	public static void main (String [] args) 
+	  {
+		Scanner input = new Scanner (System.in);
+
+		float taxa;
+		int pop2023
+
+		taxa = 1.1;
+		pop2023 = 7.7000000000000;
+
+		System.out.print ("Informe o numero da populacao mundial: ");
+		pop2023 = input.nextInt();
+		
+		
+
+		
+
+		
