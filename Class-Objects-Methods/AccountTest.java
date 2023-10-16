f36b10d923b3c3730286cc8fac72fa9c5f7ab963
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-16 22:49:22 +0200

    incomplete programm

---
+++ Class-Objects-Methods/AccountTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AccountTest
+{
+	public static void main (Strig [] args)
+	{
+		Account account1 = new Account;
+		Account account2 - new Account;
+
+		System.out.printf ("5 Balance: $%.2f%n",account1.getName(), 
+				account1.getBalance());
+		System.out.printf ("%s balance: $%.2f %n%n",account2.getName(),
+				account2.getBalance());
+
+		Scanner input = new Scanner (System.in);
+
+		System.out.print( "Enter deposit amount for account1: ");
+		double.out.printf  ("%nadding %.2f to account1 balance", depositAmount);
+		
+		System.out.printf( "%n Adding %.2f to Account1 balance%n%n", depositAmount);
+
+		account1.deposit(depositAmount);
+
+
