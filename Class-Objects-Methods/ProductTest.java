d4f588c794c0e16659cec98dba83e69c853f830e
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-21 22:26:31 +0200

    Product of 3 numbers using JOptionPane

---
+++ Class-Objects-Methods/ProductTest.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+
+public class ProductTest () 
+{
+	public static void main (String [] args)
+	{
+		Product prod = new Product(1,1,1);
+
+	JOptionPane.showInputDialog ("Enter first number: ");
+	prop.getN1() = input.parseInt();
+
+	}
+
+}
