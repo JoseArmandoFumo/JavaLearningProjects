7cfb48be7ae230da598bad006ba76ea348bce1c8
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-23 21:10:37 +0200

    Using Javax swing instead of Scanner

--- Class-Objects-Methods/ProductTest.java
+++ Class-Objects-Methods/ProductTest.java
@@ -7,9 +7,9 @@ public class ProductTest
 	{
 		Product prod = new Product(1,1,1);
 
-		JOptionPane.showMessageDialog(null, prod.getN1());
-//	JOptionPane.showInputDialog ("Enter first number: ");
-//	prod.getN1() = JOptionPane.input.parseInt();
+//		JOptionPane.showMessageDialog(null, prod.getN1());
+	int number = Integer.parseInt (JOptionPane.showInputDialog ("Enter first number: "));
+		JOptionPane.showMessageDialog(null, "NUmero1",getN1(number));
 
 	}
 
--- Class-Objects-Methods/StudentTest.java
+++ Class-Objects-Methods/StudentTest.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class StudentTest
 {
@@ -8,19 +9,25 @@ public class StudentTest
 
 		Student student=new Student();
 
-		System.out.print("Please enter the name: ");
-		String theName = input.nextLine();
+	//	System.out.print("Please enter the name: ");
+	//	String theName = input.nextLine();
+	//	student.setName(theName);
+
+		String theName = JOptionPane.showInputDialog ("Please Enter the Name: ");
 		student.setName(theName);
 
-		System.out.print("Please Enter the Age: ");
-		int theAge = input.nextInt();
+		int theAge = Integer.parseInt(JOptionPane.showInputDialog("Please Enter the Age: "));
+	//	int theAge = input.nextInt();
 		student.setAge(theAge);
 		if(theAge<0) 
-			System.out.println("The Age introduced is invalid!!");
+	//		System.out.println("The Age introduced is invalid!!");
+			JOptionPane.showInputDialog("The Age introduced is invalid!");
 		else
 		{
-		System.out.printf("The name in student object is: %s%n",student.getName());
-		System.out.printf("The age in student Object is: %d%n", student.getAge());
+		JOptionPane.showMessageDialog(null, student.getName() +"\n" + student.getAge());
+		//JOptionPane.showInputDialog(null, student.getAge());
+		//System.out.printf("The name in student object is: %s%n",student.getName());
+		//System.out.printf("The age in student Object is: %d%n", student.getAge());
 		}
 	}
 }
