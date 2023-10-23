26362ac04733b024d06adfe9351c6c4c356bab18
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-23 22:47:49 +0200

    bug fixed: final version

--- Class-Objects-Methods/ProductTest.java
+++ Class-Objects-Methods/ProductTest.java
@@ -6,17 +6,17 @@ public class ProductTest
 	public static void main (String [] args)
 	{
 		Product prod = new Product(4,2,3);
-
+		 int number;
 //		JOptionPane.showMessageDialog(null, prod.getN1());
-		int number = Integer.parseInt (JOptionPane.showInputDialog ("Enter first number: "));
+		 number = Integer.parseInt (JOptionPane.showInputDialog ("Enter first number: "));
 		prod.setN1(number);
 		
 
-		int nb = Integer.parseInt (JOptionPane.showInputDialog("Enter sen=cond Number :"));
-		prod.setN2(nb);
+		number = Integer.parseInt (JOptionPane.showInputDialog("Enter sen=cond Number :"));
+		prod.setN2(number);
 
-		int nm = Integer.parseInt (JOptionPane.showInputDialog("Enter  third number: "));
-		prod.setN3(nm);
+		number = Integer.parseInt (JOptionPane.showInputDialog("Enter  third number: "));
+		prod.setN3(number);
 
 		int product = prod.getN1()*prod.getN2()*prod.getN3();
 
