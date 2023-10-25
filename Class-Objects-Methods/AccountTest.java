4f1b60647e428297b2989f934e9a854871d4da96
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-10-25 23:55:57 +0200

    Method withdraw using nested ifs

--- Class-Objects-Methods/AccountTest.java
+++ Class-Objects-Methods/AccountTest.java
@@ -6,7 +6,7 @@ public class AccountTest
 	public static void main (String [] args)
 	{
 		Account account1 = new Account("Benk: ", 50.0); 
-		Account account2 = new Account ("Jose :",50.0);
+		Account account2 = new Account ("Jose :",70.0);
 
 			double amount = 0.0;
 		
@@ -21,11 +21,35 @@ public class AccountTest
 
 
 		JOptionPane.showMessageDialog (null, "**********************MENU ************************************** \n");
-		int op = Integer.parseInt(JOptionPane.showInputDialog ("1 . Balance : \n 2. Deposit:  \n 3. Withdraw:  \n Select the Option: "));
+		int op = Integer.parseInt(JOptionPane.showInputDialog ("1 . Withdram : \n 2. Deposit:  \n 3.Balance:  \n Select the Option: "));
 
+		if (op == 1)
+		{
 		amount = Double.parseDouble(JOptionPane.showInputDialog( "Enter withdrawal amount for account1: "));
 		//double amount = input.nextDouble;
 		account1.withdraw(amount);
+		amount = Double.parseDouble (JOptionPane.showInputDialog("Enter withdrawal amount for account2: "));
+		account2.withdraw(amount);
+
+	/*	else
+		   {
+			if(op == 2)
+			{
+				amount = Double.parseDouble(JOptionPane.showInputDialog("Enter the deposit amount: "));
+				account1.deposit(amount);
+				amount = Double.parseDouble (JOptionPane.showInputDialog("Enter the deposit amount: "));
+				account2.deposit(ammount);
+			else
+			   {
+				if (op == 3)
+				{
+					JOptionPane.showMessageDialog(null, "The Balance in Account1: " + account1.getName()+account1.getBalance()+ "\n"
+							+"Balance in account2: " + account2.getName()+ account2.getBalance() +"\n");
+				}
+			   }
+		        }
+		    }   */
+		}
 		
 		// amount = Double.ParseDouble ( JOptionPane.showInputDialog("Enter the Deposit amount: "));
 		//System.out.printf  ("%nadding %.2f to account1 balance%n", depositAmount);
