0da22b64888edbacbe5f85e33e9dc630f54958e0
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-11-23 17:58:57 -0300

    feat: average calculator

---
+++ Loops/StudentAverage.java
@@ -0,0 +1,56 @@
+import javax.swing.JOptionPane;
+
+public class StudentAverage 
+{
+	private String name;
+	private String grade;
+	//private double average;
+	//private 
+	//
+	public StudentAverage (String name, String grade)
+	{
+		this.name = name;
+		this.grade = grade;
+
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getGrade()
+	{
+		return grade;
+	}
+	public void setGrade (String grade)
+	{
+		this.grade = grade;
+	}
+
+	public double calcAverage (double test)
+		
+	{
+		double sum =0;
+		double i=0;
+		double average =0.0;
+
+		//est = Double.parseDouble(JOptionPane.showInputDialog("Introduza a primeira nota: "));
+
+		while (test >=0)
+		{
+		sum += test;
+		i = i+1;
+		test = Double.parseDouble( JOptionPane.showInputDialog("Enter test: "));
+		}
+
+		average = sum/i;
+
+		return average;
+	}
+}
+
---
+++ Loops/StudentAverageTest.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class StudentAverageTest
+{
+	public static void main (String []args )
+	{
+		String name;
+		String grade;
+		double test=0.0;
+
+		StudentAverage student = new StudentAverage ("Benk", "Pass");
+
+		name = JOptionPane.showInputDialog ("Enter your full Name: ");
+		student.setName(name);
+
+		test = Double.parseDouble (JOptionPane.showInputDialog("Initial Test : "));
+
+		double a =0.0;
+		a = student.calcAverage(test);
+
+		JOptionPane.showMessageDialog (null, "Average: "+ a);
+
+	}
+
+}
+
