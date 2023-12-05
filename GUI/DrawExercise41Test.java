8ebfc4bd9c130e84d5f44c8ae40b792e15bb4d4a
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2023-12-05 17:06:43 -0300

    Gui first

---
+++ GUI/DrawExercise41Test.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class DrawExercise41Test 
+{
+	public static void main (String [] args)
+	{
+		DrawExercise41 panel = new DrawExercise41 ();
+
+		JFrame app = new JFrame ();
+
+		app.add (panel);
+		app.setSize (250, 250);
+		app.setVisible (true);
+
+	}
+
+}
