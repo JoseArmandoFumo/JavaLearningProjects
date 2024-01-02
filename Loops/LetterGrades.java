3a68c28fe1f1806c5e4fedfda3572b5464a8b880
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2024-01-02 22:42:49 +0200

    Switch v1

---
+++ Loops/LetterGrades.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class LetterGrades
+{
+	public static void main (String [] args)
+	{
+		int total = 0;
+		int i = 0;
+		int aCounter = 0;
+		int bCOntrer = 0;
+		int cCounter = 0;
+		int dCounter = 0;
+		int eCounter = 0;
+
+	}
+
+}
