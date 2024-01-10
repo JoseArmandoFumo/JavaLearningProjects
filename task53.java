ba3491da6073a54a5d4d554feea1b6492ddf7caa
JoseArmandoFumo <benkmacanhizzofumo@gmail.com>
2024-01-10 23:59:06 +0200

    par/impar

---
+++ task53.java
@@ -0,0 +1,24 @@
+import javax.swing.JOPtionPane;
+
+
+public class Task53 
+{
+	public static void main (String [] args)
+	{
+		int i;
+		int sum;
+		int count;
+
+		for (i = 1; i<= 99; i++)
+		{
+			if (i%2 = 0 )
+			{
+				count += 1;
+				sum += i;
+			}
+
+		}
+		JOptionPane.showMessageDialog (null, "The Sum is: " + sum);
+	}
+}
+
